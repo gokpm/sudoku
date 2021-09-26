f00b3abad8c67eb76a0418918df3bc96968f1d95
Gokul PM <contact@gokulpm.com>
2021-09-26 17:53:34 +0530

    doc v1.0

--- run.py
+++ run.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     z = 1
     while z<101:
         sudoku = genSudoku(3)
+        puzzle = genPuzzle(sudoku, 25)
+        solved = solveSudoku(puzzle)
         print(z, conformityCheck(sudoku))
+        print(z, conformityCheck(solved))
         z += 1
 
         
